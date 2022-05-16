0076f41fce0d8d0e21d54720fcaf83fd5106bfc0
pc <markonikolic1081@gmail.com>
2022-05-16 12:04:24 +0200

    inputs, chackbox and radio button in html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { FooterComponent } from './footer/footer.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./homepage.component.scss']
 })
 export class HomepageComponent implements OnInit {
+  inputImeIPrezime: String = '';
+  inputEmail: String = '';
+  inputTelefon: String = '';
+  inputZemljaIGrad: String = '';
+
 
   constructor() { }
 
   ngOnInit(): void {
+    //console.log(this.inputImeIPrezime,this.inputEmail,this.inputTelefon,this.inputZemljaIGrad);
   }
+  showInputs() {
+    console.log(" Ime i prezime: " + this.inputImeIPrezime,"\n Email: " + this.inputEmail,"\n Telefon: " + this.inputTelefon,"\n Zemlja i grad: " + this.inputZemljaIGrad);
+  }
+
 
 }
