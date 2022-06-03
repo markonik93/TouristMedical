ada135b5f31e92b11a516a9f0e25e1506585ca7e
pc <markonikolic1081@gmail.com>
2022-06-03 15:36:30 +0200

    ispravka service

--- src/app/order.service.ts
+++ src/app/order.service.ts
@@ -1,15 +1,21 @@
 import { Injectable } from '@angular/core';
+import { OrderForm } from './orderForm';
 @Injectable({
   providedIn: 'root'
 })
 export class OrderService {
   orderForms:any[]=[];
+  //orderForm:OrderForm;
 
   constructor() { }
 
-  putDataInLS(orderForms:any[]){
-    console.log(orderForms);
-    localStorage.setItem('myOrders', JSON.stringify(orderForms));
+  putDataInLS(orderForm:OrderForm){
+    console.log(orderForm);
+    if(this.orderForms==null){
+      this.orderForms=new Array();
+    }
+    this.orderForms.push(orderForm); 
+    localStorage.setItem('myOrders', JSON.stringify(this.orderForms));
   }
 
   getDataFromLS(){
